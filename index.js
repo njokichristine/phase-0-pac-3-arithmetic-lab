9ba006f4644ec5857e56080d5a3f0577fcf84cc8
njokichristine <christinenjoki6900@gmail.com>
2022-12-01 16:37:24 +0300

    done

---
+++ index.js
@@ -0,0 +1,47 @@
+function add(){}
+
+function subtract(){}
+
+function multiply(){}
+
+function divide(){}
+
+function add(a,b){
+    return a+b;
+}
+add(1,2);
+
+function subtract(a,b){
+    return a-b;
+}
+add(1,2)
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+divide(4,2)
+
+function increment(n){
+    return(n+=1);
+}
+increment(1);
+
+function decrement(n){
+    return(n-=1);
+}
+decrement(11);
+
+function makeInt(string){
+  return  (parseInt(string, 10));
+}
+makeInt();
+
+function preserveDecimal(string){
+    return (parseFloat(string));
+}
+preserveDecimal();
+Footer
